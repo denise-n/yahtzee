931343ce4cbfced9f5f89e1faf8e6289795b365b
Denise <denise.ndalamba@gmail.com>
2019-10-03 12:56:10 -0400

    add descriptions to each rule

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,10 +3,12 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
+    const { name, score, doScore, description } = this.props
+   
     return (
-      <tr className={this.props.score === undefined ? "RuleRow RuleRow-active" : "RuleRow RuleRow-disabled"} onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr className={score === undefined ? "RuleRow RuleRow-active" : "RuleRow RuleRow-disabled"} onClick={doScore}>
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score === undefined ? description : score}</td>
       </tr>
     )
   }
